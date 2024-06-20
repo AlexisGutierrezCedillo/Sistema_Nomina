29c9f342fd45ae5c6469cd7b8fb74036294240d0
AlexisGutierrezCedillo <alexis.gtz.cedillo@gmail.com>
2024-06-20 01:46:38 -0600

    Versión 1
    Creación del proyecto "Sistema_Nomina" con ASP.NET MVC (Modelo Vista Controlador)
    
    Grupo: 4NM41
    Representante: Gutiérrez Cedillo Alexis Eduardo

--- obj/Debug/net8.0/Sistema_Nomina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema_Nomina.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_Nomina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5cfed6267658d87da9d48204912081aefbfaf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_Nomina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_Nomina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
